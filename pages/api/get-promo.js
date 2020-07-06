0d267f5434076e8fda9cfde1a1c02d3331b8705e
andermsiva <andermsiva78@users.noreply.github.com>
2020-07-05 22:40:17 -0300

    version002

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -1,5 +1,5 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet'
-import credentials  from '../../credentials.json'
+
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 export default async(req,res)=>{
