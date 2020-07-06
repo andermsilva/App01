2a56cab592592edf65074f02388a07ad08213120
andermsiva <andermsiva78@users.noreply.github.com>
2020-07-05 22:10:05 -0300

    initial version

---
+++ pages/sobre.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from   'next/link'
+import PageTitle from '../components/PageTitle'
+
+const Sobre =()=>{
+    return(
+        <di>
+           <PageTitle title='Sobre'/>
+            <h1>Sobre</h1>
+         </di>
+)}
+
+export default Sobre
